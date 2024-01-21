5816732f071995d813ef789e56b9452fda09ce03
doneni <xxerrenn@icloud.com>
2024-01-20 23:32:12 -0800

    pcap open

--- main.cpp
+++ main.cpp
@@ -1,8 +1,39 @@
 #include "main.h"
 
+void usage()
+{
+    printf("syntax : beacon-flood <interface> <ssid-list-file>\n");
+    printf("sample : beacon-flood mon0 ssid-list.txt\n");
+}
+
+typedef struct {
+	char* dev_;
+} Param;
+
+Param param = {
+	.dev_ = NULL
+};
+
+bool parse(Param* param, int argc, char* argv[]) {
+	if (argc != 3) {
+		usage();
+		return false;
+	}
+	param->dev_ = argv[1];
+	return true;
+}
+
 int main(void)
 {
-    printf("hello world!\n");
+    if (!parse(&param, argc, argv))
+		return -1;
+
+	char errbuf[PCAP_ERRBUF_SIZE];
+	pcap_t* pcap = pcap_open_live(param.dev_, BUFSIZ, 1, 1000, errbuf);
+	if (pcap == NULL) {
+		fprintf(stderr, "pcap_open_live(%s) return null - %s\n", param.dev_, errbuf);
+		return -1;
+    }
 
     return 0;
 }
